f75435918b0baf20bc21b92eb0702c06215340ca
Xpreme-dev1 <icarooagen@gmail.com>
2026-02-25 19:39:49 -0300

    first commit

---
+++ src/main/java/login/Login.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package login;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author 232.003479
+ */
+@WebServlet("/login")
+public class Login extends HttpServlet{
+    
+    private static final long serialVersionUID = 1L;
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    
+        String usuario = request.getParameter("users");
+        String senha = request.getParameter("passw");
+        
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        if("admin".equals(usuario) && "1234".equals(senha)){
+           response.sendRedirect("pages/home.html");
+            //out.println("<h2>Login realizado<h2>");
+        }else{
+            out.println("<h2>Usuário ou senha incorreto.<h2>");
+        }
+        
+    }  
+}
